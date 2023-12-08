ef0b539b524aadc18aeb34c3d393674d4100ecbe
Omar Bonnet <omar.bonnet@gmail.com>
2023-12-08 11:18:11 -0500

    removed unnecessary imports

--- src/main/java/org/wecancodeit/virtualpet4/repositories/ShelterRepository.java
+++ src/main/java/org/wecancodeit/virtualpet4/repositories/ShelterRepository.java
@@ -1,9 +1,7 @@
 package org.wecancodeit.virtualpet4.repositories;
 
-import java.util.Collection;
 
 import org.springframework.data.repository.CrudRepository;
-import org.wecancodeit.virtualpet4.models.PetModel;
 import org.wecancodeit.virtualpet4.models.ShelterModel;
 
 public interface ShelterRepository  extends CrudRepository<ShelterModel, Long>{
